8d4bb390c8864648ac9f776378da753693f51a8f
Bilal Rizwan <b1l4l.hustle@gmail.com>
2026-06-09 16:38:12 +0500

    Make website responsive

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -161,7 +161,7 @@ export default function DashboardLayout() {
         </div>
       </header>
 
-      <main className="relative z-10 mx-auto max-w-7xl px-6 py-8">
+      <main className="relative z-10 mx-auto max-w-7xl px-4 py-6 md:px-6 md:py-8">
         <Outlet />
       </main>
     </div>
